de899954f5bd57d4ce8f2fe353df3513ebcffb5f
Piyush Gupta <piyush@hypertrack.io>
2017-05-08 18:11:01 +0530

    Initial commit

---
+++ app/src/main/java/hypertrack/com/androidsdkonboarding/LoginActivity.java
@@ -0,0 +1,10 @@
+package hypertrack.com.androidsdkonboarding;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by piyush on 08/05/17.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+}
