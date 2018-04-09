675181763b40756216fde9206697bf27f987d033
Abhishek Poddar <abhishek@hypertrack.io>
2018-04-09 16:58:58 +0530

    Update MyApplication.java

--- app/src/main/java/com/hypertrack/androidsdkonboarding/MyApplication.java
+++ app/src/main/java/com/hypertrack/androidsdkonboarding/MyApplication.java
@@ -1,15 +1,10 @@
 package com.hypertrack.androidsdkonboarding;
-
 import android.app.Application;
-
 import com.hypertrack.lib.HyperTrack;
-
 public class MyApplication extends Application {
-
     @Override
     public void onCreate() {
         super.onCreate();
-
         // Initialize HyperTrack SDK with your Publishable Key here.
         // You need to sign up to get your keys: 
         // https://www.hypertrack.com/signup
