62533aaa814347893af0c6dd9258b8e55cd27a45
Abhishek Poddar <abhishek@hypertrack.io>
2018-04-09 17:00:30 +0530

    Update onboarding-content.js

--- onboarding-content.js
+++ onboarding-content.js
@@ -39,7 +39,7 @@
 			"fileURL": "https://raw.githubusercontent.com/hypertrack/android-sdk-onboarding/master/app/src/main/java/com/hypertrack/androidsdkonboarding/MyApplication.java",
 			"lines": [{
 				"start": 1,
-				"end": 6
+				"end": 3
 			}]
 		}
 	]
